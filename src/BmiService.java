caacb204dc9b953f87f4626bf4dc91d3aca12377
Alena Nik <alyona_nik@mail.ru>
2023-04-14 14:39:57 +0500

    first commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+        public double calculate(double height_cm, double weight_kg) {
+            double height = height_cm / 100;
+            double index = weight_kg/(height*height);
+            return index;
+        }
+    }
