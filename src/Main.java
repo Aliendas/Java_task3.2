ac039b76a08796064c709b79f68b78811311c8c5
Alena Nik <alyona_nik@mail.ru>
2023-04-19 12:33:54 +0400

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int bmi = (int) service.calculate(187,98);
+        int bmi = (int) service.calculate(187, 98);
         System.out.println("Индекс массы тела = " + bmi);
     }
 }
